6a07a0fb766d43189ee1c576c04a1b010a68e6d2
dariush kadkhoda <dariushkadkhoda@gmail.com>
2023-02-23 07:42:25 -0800

    first commit

---
+++ src/sections/about/About.js
@@ -0,0 +1,12 @@
+import './about.css'
+
+const About = () => {
+    return (
+        <section id="about">
+            <h2>about</h2>
+
+        </section>
+    );
+}
+
+export default About;
---
+++ src/sections/navbar/Navbar.js
@@ -0,0 +1,10 @@
+import './navbar.css'
+const Navbar = () => {
+    return (
+        <section id="navbar">
+            <h2>navbar</h2>
+        </section>            
+    );
+}
+
+export default Navbar;
