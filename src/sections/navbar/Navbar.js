524e714bac098a72848274317e0a721f23c7ecd5
dariush kadkhoda <dariushkadkhoda@gmail.com>
2023-02-24 08:36:30 -0800

    navbar

--- src/sections/navbar/Navbar.js
+++ src/sections/navbar/Navbar.js
@@ -1,9 +1,24 @@
 import './navbar.css'
+import data from './data';
+import logo from '../../assets/image/logo.png'
+import {IoIosColorPalette} from 'react-icons/io'
 const Navbar = () => {
     return (
-        <section id="navbar">
-            <h2>navbar</h2>
-        </section>            
+        <nav>
+            <div className="container nav__container">
+                <a href="index.html" className='nav__logo'>
+                    <img src={logo} alt="" />
+                </a>
+                <ul className='nav__menu'>
+                    {data.map(item => 
+                        <li key={item.id}><a href={item.link}>{item.title}</a></li>
+                    )}
+                </ul>
+                <button id='theme__icon'>
+                    <IoIosColorPalette/>
+                </button>
+            </div>
+        </nav>
     );
 }
 
