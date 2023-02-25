57302e7f7000c314a08e6caba723a1b92ffe6c00
dariush kadkhoda <dariushkadkhoda@gmail.com>
2023-02-25 06:50:17 -0800

    header

--- src/sections/header/Header.js
+++ src/sections/header/Header.js
@@ -1,9 +1,30 @@
 import './header.css'
+import HeaderImage from '../../assets/image/men.jpg'
+import data from '../header/data';
+
 const Header = () => {
     return (
-        <section id="header">
-            <h2>header</h2>
-        </section>
+        <header id="header">
+            <div className="container header__container">
+                <div className="header__profile">
+                    <img src={HeaderImage} alt="header_profile" />
+                </div>
+                <h3>Dariush Kadkhoda</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dicta magnam placeat mollitia! Labore, fuga quibusdam
+                    possimus reprehenderit laborum fugitpossimus reprehenderit laborum fugit
+                </p>
+                <div className="header__cta">
+                    <a href="#contact" className='btn primary'>Let`s Talk</a>
+                    <a href="#portfolio" className='btn light'>My work</a>
+                </div>
+                <div className='header__socials'>
+                    {
+                        data.map(item => <a key={item.id} href={item.link} target="_blank" rel='noopener noreferrer'>{item.icon}</a>)
+                    }
+                </div>
+            </div>
+        </header>
     );
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,13 @@
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiOutlineWhatsApp} from 'react-icons/ai'
+import {AiFillGithub} from 'react-icons/ai'
+
+const Data =[
+    {id:1,link:'https://instagram.com',icon:<AiOutlineInstagram/>},
+    {id:2,link:'https://twitter.com',icon:<AiOutlineTwitter/>},
+    {id:3,link:'https://whatsapp.com',icon:<AiOutlineWhatsApp/>},
+    {id:5,link:'https://githab.com',icon:<AiFillGithub/>},
+]
+
+export default Data
