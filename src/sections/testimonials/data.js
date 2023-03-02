1b580dfa5f3411307eef02e765b781a46d231db5
dariush kadkhoda <dariushkadkhoda@gmail.com>
2023-03-02 04:21:21 -0800

    testimonials

---
+++ src/sections/testimonials/data.js
@@ -0,0 +1,54 @@
+import Avatar from '../../assets/image/avatar.jpg'
+
+const data = [
+    {
+        id:1,
+        quote :"   Excepturi quibusdam sed accusamus dolor quis impedit, ad labore reprehenderit magnam.",
+        avatar:Avatar,
+        name :'dariush kadkhoda',
+        profession:'Actress'
+    },
+    {
+        id:2,
+        quote :" Lorem ipsum, dolor sit amet consectetur adipisicing elit.  Excepturi quibusdam sed accusamus dolor quis impedit, ad labore reprehenderit magnam.",
+        avatar:Avatar,
+        name :'dariush kadkhoda',
+        profession:'Actress'
+    },
+    {
+        id:3,
+        quote :" Lorem ipsum, dolor sit amet consectetur adipisicing elit.  ad labore reprehenderit magnam.",
+        avatar:Avatar,
+        name :'dariush kadkhoda',
+        profession:'Actress'
+    },
+    {
+        id:4,
+        quote :"  fugiat dignissimos. Excepturi quibusdam sed accusamus dolor quis impedit, ad labore reprehenderit magnam.",
+        avatar:Avatar,
+        name :'dariush kadkhoda',
+        profession:'Actress'
+    },
+    {
+        id:5,
+        quote :" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia ullam consequatur quo animi veritatis a in deleniti fugiat dignissimos..",
+        avatar:Avatar,
+        name :'dariush kadkhoda',
+        profession:'Actress'
+    },
+    {
+        id:6,
+        quote :"  Excepturi quibusdam sed accusamus dolor quis impedit, ad labore reprehenderit magnam.",
+        avatar:Avatar,
+        name :'dariush kadkhoda',
+        profession:'Actress'
+    },
+    {
+        id:7,
+        quote :"  Excepturi quibusdam sed accusamus dolor quis impedit, ad labore reprehenderit magnam.",
+        avatar:Avatar,
+        name :'dariush kadkhoda',
+        profession:'Actress'
+    },
+]
+export default data
